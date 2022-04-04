318cd18cc404b3690af060455215c092cb60b8db
Arthur Santos <arthur.santos@genyx.com.br>
2022-04-04 20:15:31 -0300

    Tela inicial pronta e funcionando

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-let telas = ['componente1', 'componente2']
+let telas = ['componente-0',
+    'componente1', 'componente2'
+];
+
+let petshops = [
+    { id: 1, name: 'CasaPet', coordinates: { lat: -15.9120, lng: -53.1492 } },
+    { id: 2, name: 'Petcila', coordinates: { lat: -15.9120, lng: -53.1492 } },
+    { id: 3, name: 'Real Pet', coordinates: { lat: -15.9120, lng: -53.1492 } },
+    { id: 4, name: 'Pet Nosso', coordinates: { lat: -15.9120, lng: -53.1492 } },
+    { id: 5, name: 'Pet Store', coordinates: { lat: -15.9120, lng: -53.1492 } }
+];
+
+let servicos = [
+    { id: 1, name: 'Banho' },
+    { id: 2, name: 'Tosa' },
+    { id: 3, name: 'Banho e Tosa' },
+    { id: 4, name: 'Pesticida' },
+    { id: 5, name: 'Veterinário' },
+    { id: 6, name: 'Hotelaria' },
+]
 
 const mostraComp = (comp) => {
     telas.forEach((c) => {
@@ -13,14 +32,79 @@ const ativaElem = (elem) => {
     elem.classList.add('active')
 }
 
+const ExibeListaPetshop = () => {
+    const listaPetshop = document.querySelector('#listaPetshops')
+    listaPetshop.innerHTML = '';
+    petshops.forEach((p) => {
+        let elemPetshop = document.createElement('li');
+        elemPetshop.innerHTML = p.name;
+        elemPetshop.classList.add('light');
+        elemPetshop.onclick = () => {
+            // editar/visualizar petshop
+            mostraComp('componente3');
+        }
+        listaPetshop.appendChild(elemPetshop);
+    });
+    if (petshops.length > 0) {
+        listaPetshop.classList.remove('hidden');
+        document.querySelector('#blank').classList.add('hidden')
+    } else {
+        listaPetshop.classList.add('hidden');
+        document.querySelector('#blank').classList.remove('hidden')
+    }
+}
+
+const MostraCamposNovoServico = () => {
+    document.querySelector('#divFieldServicoPet').classList.remove('hidden');
+    document.querySelector('#ulListaServicoPet').classList.add('hidden');
+    document.querySelector('#divSalvarPet').classList.add('hidden');
+    document.querySelector('#iptNomePet').focus();
+}
+
+const AddNovoServico = (obj) => {
+    const ulListaServicoPet = document.querySelector('#ulListaServicoPet')
+    ulListaServicoPet.innerHTML = '';
+
+    // servicos.forEach((p) => {
+    //     let elemS = document.createElement('li');
+    //     elemS.innerHTML = p.name;
+    //     ulListaServicoPet.appendChild(elemS);
+
+    // });
+
+    document.querySelector('#divFieldServicoPet').classList.add('hidden');
+    document.querySelector('#ulListaServicoPet').classList.remove('hidden');
+    document.querySelector('#divSalvarPet').classList.remove('hidden');
+}
+
+const VoltaPetshop = () => {
+    document.querySelector('#iptNome').value = '';
+    document.querySelector('#iptValor').value = '';
+    document.querySelector('#divFieldServicoPet').classList.add('hidden');
+    document.querySelector('#ulListaServicoPet').classList.remove('hidden');
+    document.querySelector('#divSalvarPet').classList.remove('hidden');
+}
+
 onload = () => {
-    document.querySelector('#markPethop').onclick = (e) => {
+    ExibeListaPetshop();
+
+    /* Novo Petshop */
+    document.querySelector('#btnNovoPetshop').onclick = (e) => {
         mostraComp('componente2');
     }
-    document.querySelector('#btnCloseSch').onclick = (e) => {
-        mostraComp('componente1');
+
+
+    /* Editar Petshop */
+    document.querySelector('#btnNewServicePet').onclick = (e) => {
+        MostraCamposNovoServico();
     }
-    document.querySelector('#btnCancelSch').onclick = (e) => {
+    document.querySelector('#btnVoltarPet').onclick = (e) => {
+        VoltaPetshop();
+    }
+    document.querySelector('#btnAddServicePet').onclick = (e) => {
+        AddNovoServico();
+    }
+    document.querySelector('#btnVoltarIni').onclick = (e) => {
         mostraComp('componente1');
     }
 }
